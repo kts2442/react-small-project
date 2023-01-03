cfa8420985e7d487e792a450abae7aedb89d261e
kts2442 <https://github.com/kts2442>
2023-01-03 21:14:34 +0900

    react_movie_app : develop Detail routes and use 'useParams'

--- react_movie/src/routes/Detail.js
+++ react_movie/src/routes/Detail.js
@@ -1,5 +1,38 @@
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import Movie from "../components/Movie";
+
 function Detail() {
-  return <h1>Detail</h1>;
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState([]);
+  const { id } = useParams();
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    setMovie([json.data.movie]);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movie.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.background_image} alt={movie.id} />
+              <h2>{movie.title}</h2>
+              <h3>Rating : {movie.rating}</h3>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Detail;
